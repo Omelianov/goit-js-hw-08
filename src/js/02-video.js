cbe3465fba7825def6aec9612983fe0b05154db7
Omelianov Nicolai <omelianov.n@gmail.com>
2023-03-09 10:09:22 +0200

    added 1 and 2 tasks

---
+++ src/js/02-video.js
@@ -0,0 +1,14 @@
+import throttle from 'lodash.throttle'
+
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const currentTime = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+    localStorage.setItem(currentTime, seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(currentTime));
